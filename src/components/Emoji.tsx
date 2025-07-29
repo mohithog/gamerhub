eeb73364e7856fa5758869e421f76ac3795cd5a3
Mohith <mohithog@gmail.com>
2025-07-29 18:26:24 +0530

    small bug fix

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -8,7 +8,7 @@ interface Props{
     rating: number;
 }
 const Emoji = ({ rating }: Props) => {
-    if (rating < 3) return;
+    if (rating < 3) return null;
 
     const emojiMap: {[key: number]: ImageProps} = {
         3: {src: meh , alt:'meh' , boxSize: '25px'},
