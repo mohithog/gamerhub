9d5f323b4b7b6855aa8d49b7539ac29a9d976db6
Mohith <mohithog@gmail.com>
2025-08-19 15:20:21 +0530

    Refactor: Simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import ms from 'ms';
 
 const apiClient = new APIClient<Genres>('/genres');
 
@@ -14,7 +15,7 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms('24h'),
     initialData: genres
   });
 };
